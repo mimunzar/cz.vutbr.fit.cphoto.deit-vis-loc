0ea0e341a7e073c494d348d1cb1661d6cce4f286
Milan Munzar <mimunzar@gmail.com>
2021-10-26 18:22:03 +0200

    Creates Triplets (A, P, N)

--- src/deit_vis_loc/main.py
+++ src/deit_vis_loc/main.py
@@ -7,11 +7,11 @@ DATASET_PATH = '.git/datasets/GeoPose3K_v2/'
 BATCH_SIZE   = 100
 
 
-def read_paths_for_queries(dpath, name):
-    queries_dpath = os.path.join(dpath, 'query_original_result')
-    spec_paths    = os.path.join(queries_dpath, name)
-    if os.path.exists(spec_paths):
-        with open(spec_paths) as f:
+def read_queries_paths(dataset_path, name):
+    queries_dpath = os.path.join(dataset_path, 'query_original_result')
+    dataset_fpath = os.path.join(queries_dpath, name)
+    if os.path.exists(dataset_fpath):
+        with open(dataset_fpath) as f:
             return [os.path.join(queries_dpath, line.strip()) for line in f]
     raise FileNotFoundError(
             'Failed to read queries paths ({} not found in {})'.format(name, queries_dpath))
@@ -21,13 +21,33 @@ def partition(n, coll):
     assert 0 < n
     if not coll:
         return []
-
     part = coll[:n]
     if len(part) == n:
         return [part] + partition(n, coll[n:])
     return [part]
 
 
+def to_segment_path(query_path):
+    return query_path \
+        .replace('query_original_result', 'query_segments_result') \
+        .replace('.jpg', '.png')
+
+
+def gen_triplets(list_of_query_paths, fn_to_segment_path):
+    result             = []
+    set_of_query_paths = set(list_of_query_paths)
+    for query_path in list_of_query_paths:
+        pos_segment = fn_to_segment_path(query_path)
+        for neg_path in set_of_query_paths - set([query_path]):
+            result.append({
+                'A': query_path,
+                'P': pos_segment,
+                'N': fn_to_segment_path(neg_path)
+            })
+    return result
+
+
 if __name__ == "__main__":
-    queries_paths = partition(BATCH_SIZE, read_paths_for_queries(DATASET_PATH, 'train.txt'))
+    list_of_batches  = partition(BATCH_SIZE, read_queries_paths(DATASET_PATH, 'train.txt'))
+    list_of_triplets = (gen_triplets(b, to_segment_path) for b in list_of_batches)
 
--- tests/deit_vis_loc/test_main.py
+++ tests/deit_vis_loc/test_main.py
@@ -11,3 +11,18 @@ def test_partition():
     assert main.partition(3, [1, 2, 3]) == [[1, 2, 3]]
     assert main.partition(4, [1, 2, 3]) == [[1, 2, 3]]
 
+
+def test_generate_triplets():
+    fn_to_segment_path = lambda s: s + '_segment'
+    assert main.gen_triplets([], fn_to_segment_path) == []
+    assert main.gen_triplets(['foo'], fn_to_segment_path) == []
+    #^ No negative samples present
+    assert main.gen_triplets(['foo', 'bar', 'baz'], fn_to_segment_path) == [
+            {'A': 'foo', 'P': 'foo_segment', 'N': 'bar_segment'},
+            {'A': 'foo', 'P': 'foo_segment', 'N': 'baz_segment'},
+            {'A': 'bar', 'P': 'bar_segment', 'N': 'foo_segment'},
+            {'A': 'bar', 'P': 'bar_segment', 'N': 'baz_segment'},
+            {'A': 'baz', 'P': 'baz_segment', 'N': 'foo_segment'},
+            {'A': 'baz', 'P': 'baz_segment', 'N': 'bar_segment'},
+        ]
+
