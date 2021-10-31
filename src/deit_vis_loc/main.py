fc20eb67d129c72ecc77b1ea0b8308c2a88c709e
Milan Munzar <mimunzar@gmail.com>
2021-10-31 10:50:36 +0100

    Moves Evaluation to GPU

--- src/deit_vis_loc/main.py
+++ src/deit_vis_loc/main.py
@@ -13,31 +13,10 @@ import src.deit_vis_loc.utils as utils
 
 DATASET_PATH = '.git/datasets/GeoPose3K_v2/'
 
-def gen_query_paths(dataset_path, name):
-    queries_dpath = os.path.join(dataset_path, 'query_original_result')
+def gen_anchor_imgs(dataset_dpath, name):
+    queries_dpath = os.path.join(dataset_dpath, 'query_original_result')
     dataset_fpath = os.path.join(queries_dpath, name)
     return (os.path.join(queries_dpath, l.strip()) for l in open(dataset_fpath))
-
-
-def embeddings(model, fn_transform, device, fpath):
-    return model(fn_transform(PIL.Image.open(fpath)).unsqueeze(0).to(device))
-
-
-def gen_embedding_distances(model, fn_transform, device, list_of_query_paths):
-    torch.set_grad_enabled(False);
-    model.eval();
-
-    list_of_seg_paths      = [utils.to_segment_path(p) for p in list_of_query_paths]
-    list_of_seg_embeddings = [embeddings(model, fn_transform, device, seg_path)
-            for seg_path in list_of_seg_paths]
-
-    for query_path in list_of_query_paths:
-        query_embedding       = embeddings(model, fn_transform, device, query_path)
-        list_of_seg_distances = [torch.cdist(query_embedding, e).item()
-                for e in list_of_seg_embeddings]
-        list_of_seg_dist_path = [{'distance': d, 'path': p}
-                for p, d in zip(list_of_seg_paths, list_of_seg_distances)]
-        yield { 'query_path': query_path, 'segments': list_of_seg_dist_path }
 
 
 def gen_triplets(list_of_anchor_imgs, fn_to_segment_img):
@@ -52,22 +31,19 @@ def gen_triplets(list_of_anchor_imgs, fn_to_segment_img):
             }
 
 
-def gen_tensor_triplets(fn_to_tensor, list_of_triplets):
-    to_tensor_triplet = lambda t: {k: fn_to_tensor(v) for k,v in t.items()}
-    return (to_tensor_triplet(t) for t in list_of_triplets)
-
-
-def gen_triplet_loss(fn_triplet_loss, fn_to_tensor, list_of_imgs):
-    list_of_triplets = list(gen_triplets(list_of_imgs, utils.to_segment_path))
+def gen_triplet_loss(fn_triplet_loss, fn_img_to_tensor, list_of_imgs):
+    list_of_triplets = list(gen_triplets(list_of_imgs, utils.to_segment_img))
     random.shuffle(list_of_triplets)
     #^ Shuffle triplets so they are not sorted by an anchor
-    return (fn_triplet_loss(t) for t in gen_tensor_triplets(fn_to_tensor, list_of_triplets))
+    to_tensor_triplet = lambda t: {k: fn_img_to_tensor(v) for k,v in t.items()}
+    return (fn_triplet_loss(tt) for tt in (to_tensor_triplet(t) for t in list_of_triplets))
 
 
-def train_one_epoch(model, optimizer, fn_triplet_loss, fn_to_tensor, list_of_imgs):
+def train_one_epoch(model, optimizer, fn_triplet_loss, fn_img_to_tensor, list_of_imgs):
     model.train()
+
     for batch_of_imgs in utils.partition(104, list_of_imgs):
-        for loss in gen_triplet_loss(fn_triplet_loss, fn_to_tensor, batch_of_imgs):
+        for loss in gen_triplet_loss(fn_triplet_loss, fn_img_to_tensor, batch_of_imgs):
             optimizer.zero_grad(); loss.backward(); optimizer.step()
 
 
@@ -87,7 +63,20 @@ def make_img_to_tensor(device):
         torchvision.transforms.ToTensor(),
         torchvision.transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD),
     ])
-    return lambda im_path: to_tensor(PIL.Image.open(im_path)).unsqueeze(0).to(device)
+    return lambda img: to_tensor(PIL.Image.open(img)).unsqueeze(0).to(device)
+
+
+def gen_evaluate_model(model, fn_img_to_tensor, list_of_anchor_imgs):
+    torch.set_grad_enabled(False);
+    model.eval();
+
+    list_of_anchor_imgs  = list(list_of_anchor_imgs)
+    list_of_segment_imgs = [utils.to_segment_img(a) for a in list_of_anchor_imgs]
+    compute_embeddings   = lambda im_path: model(fn_img_to_tensor(im_path))
+    for anchor_img in list_of_anchor_imgs:
+        a_embed = compute_embeddings(anchor_img)
+        s_dists = (torch.cdist(a_embed, compute_embeddings(s)) for s in list_of_segment_imgs)
+        yield { 'anchor': anchor_img, 'segments': zip(list_of_segment_imgs, s_dists) }
 
 
 if __name__ == "__main__":
@@ -96,11 +85,14 @@ if __name__ == "__main__":
     model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), 1e-4)
 
-    list_of_imgs = list(gen_query_paths(DATASET_PATH, 'train.txt'))
-    random.shuffle(list_of_imgs)
+    list_of_train_imgs = list(gen_anchor_imgs(DATASET_PATH, 'train.txt'))
+    random.shuffle(list_of_train_imgs)
     #^ Shuffle dataset so generated batches are different every time
 
     fn_img_to_tensor = make_img_to_tensor(device)
     fn_triplet_loss  = make_triplet_loss(device, margin=0.2)
-    train_one_epoch(model, optimizer, fn_triplet_loss, fn_img_to_tensor, list_of_imgs)
+    train_one_epoch(model, optimizer, fn_triplet_loss, fn_img_to_tensor, list_of_train_imgs)
+
+    gen_of_val_imgs = gen_anchor_imgs(DATASET_PATH, 'val.txt')
+    val = gen_evaluate_model(model, fn_img_to_tensor, gen_of_val_imgs)
 
--- src/deit_vis_loc/utils.py
+++ src/deit_vis_loc/utils.py
@@ -10,13 +10,13 @@ def partition(n, iterable):
             (list(it.islice(iterable, n)) for _ in it.repeat(None)))
 
 
-def to_segment_path(query_path):
+def to_segment_img(query_path):
     return query_path \
         .replace('query_original_result', 'query_segments_result') \
         .replace('.jpg', '.png')
 
 
-def plot_image(axis, fpath, border=None):
+def _plot_img(axis, fpath, border=None):
     import matplotlib.image as mpimg
 
     if border:
@@ -28,17 +28,15 @@ def plot_image(axis, fpath, border=None):
     axis.imshow(mpimg.imread(fpath))
 
 
-def plot_closest_distances(embedding_distance):
+def plot_closest_segments(anchor_val):
     import matplotlib.pyplot as mpplt
 
-    query_path = embedding_distance['query_path']
     _, axis  = mpplt.subplots(1, 5, constrained_layout=True)
-    plot_image(axis[0], query_path)
+    _plot_img(axis[0], anchor_val['anchor'])
 
-    list_of_segments = sorted(
-            embedding_distance['segments'], key=op.itemgetter('distance'))
+    list_of_segments = sorted(anchor_val['segments'], key=op.itemgetter(1))
     for i in range(1, 5):
-        segment_path = list_of_segments[i - 1]['path']
-        border_color = 'green' if segment_path == to_segment_path(query_path) else 'red'
-        plot_image(axis[i], segment_path, border={'color': border_color, 'width': 3})
+        segment = list_of_segments[i - 1][0]
+        border_color = 'green' if segment == to_segment_img(anchor_val['anchor']) else 'red'
+        _plot_img(axis[i], segment, border={'color': border_color, 'width': 3})
 
