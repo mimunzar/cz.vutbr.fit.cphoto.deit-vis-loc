b3b4637f10b44ba07fdb02628920e01c3119f2f1
Milan Munzar <mimunzar@gmail.com>
2021-12-27 17:34:26 +0100

    Adjust Plotting of Location Ranks

--- src/deit_vis_loc/plot.py
+++ src/deit_vis_loc/plot.py
@@ -3,9 +3,10 @@
 import matplotlib.image  as mpimg
 import matplotlib.pyplot as mpplt
 
-import itertools as it
-import math      as ma
-import operator  as op
+import collections as cl
+import itertools   as it
+import math        as ma
+import operator    as op
 
 import src.deit_vis_loc.util as util
 
@@ -21,11 +22,11 @@ def plot_img_on_axis(im, axis):
     return (im, axis)
 
 
-def ims_of_query_with_n_segments(n, name, segments_it):
+def ims_of_query_with_n_segments(n, name, segment_it):
     to_segment_img  = lambda s: (s['name'], 'green' if s['is_positive'] else 'red', s['distance'])
     to_segment_imgs = lambda l: map(to_segment_img, sorted(l, key=op.itemgetter('distance')))
     to_query_im     = lambda s: (s, 'black', 0)
-    return util.prepend(to_query_im(name), it.islice(to_segment_imgs(segments_it), n))
+    return util.prepend(to_query_im(name), it.islice(to_segment_imgs(segment_it), n))
 
 
 def n_closest_segments(n, result_it):
@@ -37,18 +38,25 @@ def n_closest_segments(n, result_it):
     return [list(it.starmap(plot_img_on_axis, row)) for row in plt_grid]
 
 
-def _index_of_anchor_segment(anchor, segments):
-    return next(i for i, s in enumerate(segments) if s['is_positive'])
+def localization_percentile(segment_it):
+    by_distancce = sorted(segment_it, key=op.itemgetter('distance'))
+    positive_idx = next(i for i, s in enumerate(by_distancce, start=1) if s['is_positive'])
+    return ma.ceil((positive_idx/len(by_distancce))*100)
 
 
-def percentage_of_localized_images(query_iterable, nsegments=6192):
-    location_rank_perc  = lambda x: x*100/nsegments
-    sorted_by_distance  = ((x['query'], sorted(x['segments'], key=op.itemgetter('distance'))) for x in query_iterable)
-    anchor_segment_ids  = (_index_of_anchor_segment(*x) for x in sorted_by_distance)
-    sorted_percentiles  = list(sorted(ma.ceil(location_rank_perc(x)) for x in anchor_segment_ids))
-    percentile_buckets  = (list(util.subseq(x, sorted_percentiles)) for x in range(1, 101))
-    percentile_lens     = it.accumulate(len(x) for x in percentile_buckets)
-    percentile_percents = [location_rank_perc(x) for x in percentile_lens]
+def running_localization_percentage(percentile_it):
+    grouped    = it.groupby(sorted(percentile_it))
+    histogram  = cl.defaultdict(lambda: 0, {k: len(list(l)) for k, l in grouped})
+    total_size = sum(histogram.values())
+    percentage = lambda n: round((n/total_size)*100, ndigits=2)
+    return [percentage(histogram[i]) for i in range(1, 101)]
+
+
+def rank_of_correct_location(result_it):
+    percentiles   = (localization_percentile(r['segments']) for r in result_it)
+    bounded_add   = lambda x, y: min(x + y, 100)
+    location_rank = list(it.accumulate(
+        running_localization_percentage(percentiles), bounded_add))
 
     _, ax = mpplt.subplots()
     ax.set_xlabel('Rank of Correct Location (%)')
@@ -59,5 +67,6 @@ def percentage_of_localized_images(query_iterable, nsegments=6192):
     ax.spines['right'].set_visible(False)
     ax.set_ylim(0, 101)
     ax.set_xlim(0, 101)
-    ax.plot(percentile_percents, linewidth=2.5)
+    ax.plot(location_rank, linewidth=2.5)
+    return location_rank
 
--- tests/deit_vis_loc/test_plot.py
+++ tests/deit_vis_loc/test_plot.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import src.deit_vis_loc.plot as plot
+import pytest
+
+import collections as cl
 
 
 def test_ims_of_query_with_n_segments():
@@ -19,6 +22,41 @@ def test_ims_of_query_with_n_segments():
             ('foo', 'black', 0), ('s1', 'green', 1), ('s2', 'red', 2), ('s3', 'red', 3)]
 
 
-def test_index_of_first_positive():
-    pass
+def test_localization_percentile():
+    with pytest.raises(Exception):
+        plot.localization_percentile([])
+    #^ Query has to always be associated with a positive segment
+
+    assert plot.localization_percentile([
+            {'distance': 1, 'is_positive': True},
+        ]) == 100
+    assert plot.localization_percentile([
+            {'distance': 2, 'is_positive': True},
+            {'distance': 1, 'is_positive': False},
+        ]) == 100
+    assert plot.localization_percentile([
+            {'distance': 1, 'is_positive': True},
+            {'distance': 2, 'is_positive': False},
+            {'distance': 3, 'is_positive': False},
+        ]) == 34
+
+
+def test_percentiles_histogram():
+    with pytest.raises(Exception):
+        plot.running_localization_percentage([])
+
+    result = plot.running_localization_percentage([1])
+    assert 100 == len(result)
+    assert 100 == result[0]
+    assert all(r == 0 for r in result[1:])
+
+    result = plot.running_localization_percentage([2, 1, 1])
+    assert 100 == len(result)
+    assert [66.67, 33.33] == result[0:2]
+    assert all(r == 0 for r in result[2:])
+
+    result = plot.running_localization_percentage([2, 1, 1, 3])
+    assert 100 == len(result)
+    assert [50, 25, 25] == result[0:3]
+    assert all(r == 0 for r in result[3:])
 
