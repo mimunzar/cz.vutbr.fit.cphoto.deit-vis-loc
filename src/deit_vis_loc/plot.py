04edd87680adb143591dcf83f6bab6cae407b195
Milan Munzar <mimunzar@gmail.com>
2022-03-07 13:52:50 +0100

    Fixes Plotting Module

--- src/deit_vis_loc/plot.py
+++ src/deit_vis_loc/plot.py
@@ -5,68 +5,71 @@ import matplotlib.pyplot as mpplt
 
 import collections as cl
 import itertools   as it
+import functools   as ft
 import math        as ma
-import operator    as op
 
 import src.deit_vis_loc.util as util
 
 
-def plot_img_on_axis(im, axis):
-    fpath, color, _ = im
+def plot_im_on_axis(im, axis):
+    fpath, color, dist = im
     for spine_pos in ['bottom','top', 'right', 'left']:
         axis.spines[spine_pos].set_color(color)
         axis.spines[spine_pos].set_linewidth(3)
     axis.xaxis.set_visible(False)
     axis.yaxis.set_visible(False)
+    if 0 != dist: axis.text(7, 28, round(dist, 4), bbox={'facecolor': 'white'})
     axis.imshow(mpimg.imread(fpath))
     return (im, axis)
 
 
-def ims_of_query_with_n_segments(n, name, segment_it):
-    to_segment_img  = lambda s: (s['name'], 'green' if s['is_positive'] else 'red', s['distance'])
-    to_segment_imgs = lambda l: map(to_segment_img, sorted(l, key=op.itemgetter('distance')))
-    to_query_im     = lambda s: (s, 'black', 0)
-    return util.prepend(to_query_im(name), it.islice(to_segment_imgs(segment_it), n))
+def im_and_n_closest_segments(n, name, segment_it):
+    to_query_im    = lambda s: (s, 'black', 0)
+    to_segment_img = lambda s: (s['segment'], 'green' if s['is_pos'] else 'red', s['dist'])
+    return util.prepend(to_query_im(name), util.take(n, map(to_segment_img, segment_it)))
 
 
-def n_closest_segments(n, result_it):
-    im_rows  = [ims_of_query_with_n_segments(n, r['query'], r['segments']) for r in result_it]
+def plot_n_closest_segments(n, results_it):
+    rows_it  = tuple(it.starmap(ft.partial(im_and_n_closest_segments, n), results_it))
     figure   = mpplt.figure(constrained_layout=True)
-    im_grid  = figure.add_gridspec(nrows=len(im_rows), ncols=n + 1, hspace=0.05, wspace=0.05)
-    axis     = lambda r, c: figure.add_subplot(im_grid[r, c])
-    plt_grid = (zip(ims, (axis(r, c) for c in it.count())) for r, ims in enumerate(im_rows))
-    return [tuple(it.starmap(plot_img_on_axis, row)) for row in plt_grid]
+    im_grid  = figure.add_gridspec(nrows=len(rows_it), ncols=n + 1, hspace=0.05, wspace=0.05)
+    plot_im  = lambda r, c, im: plot_im_on_axis(im, figure.add_subplot(im_grid[r, c]))
+    plot_row = lambda r, im_it: tuple(it.starmap(ft.partial(plot_im, r), enumerate(im_it)))
+    return tuple(it.starmap(plot_row, enumerate(rows_it)))
 
 
 def localization_percentile(segment_it):
-    by_distancce = sorted(segment_it, key=op.itemgetter('distance'))
-    positive_idx = next(i for i, s in enumerate(by_distancce, start=1) if s['is_positive'])
-    return ma.ceil((positive_idx/len(by_distancce))*100)
+    segment_it = tuple(segment_it)
+    pos_idx    = next(i for i, s in enumerate(segment_it, start=1) if s['is_pos'])
+    return ma.ceil((pos_idx/len(segment_it))*100)
 
 
-def running_localization_percentage(percentile_it):
-    grouped    = it.groupby(sorted(percentile_it))
-    histogram  = cl.defaultdict(lambda: 0, {k: len(tuple(l)) for k, l in grouped})
-    total_size = sum(histogram.values())
-    percentage = lambda n: round((n/total_size)*100, ndigits=2)
-    return [percentage(histogram[i]) for i in range(1, 101)]
+def histogram(iterable):
+    grouped = it.groupby(sorted(iterable))
+    return cl.defaultdict(lambda: 0, it.starmap(lambda k, v: (k, len(tuple(v))), grouped))
 
 
-def rank_of_correct_location(result_it):
-    percentiles   = (localization_percentile(r['segments']) for r in result_it)
+def iter_histogram_perc(hist, rng):
+    total_size = sum(hist.values())
+    if 0 == total_size: return iter(())
+    return map(lambda i: round((hist[i]/total_size)*100, ndigits=2), rng)
+
+
+def plot_rank_of_correct_location(results_it):
+    percentiles   = map(localization_percentile, map(util.second, results_it))
     bounded_add   = lambda x, y: min(x + y, 100)
     location_rank = tuple(it.accumulate(
-        running_localization_percentage(percentiles), bounded_add))
+        iter_histogram_perc(histogram(percentiles), range(1, 101)), bounded_add))
 
-    _, ax = mpplt.subplots()
-    ax.set_xlabel('Rank of Correct Location (%)')
-    ax.set_ylabel('% of Queries')
-    ax.set_title('Localization Results')
-    ax.grid(True, linestyle='--', linewidth=2)
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.set_ylim(0, 101)
-    ax.set_xlim(0, 101)
-    ax.plot(location_rank, linewidth=2.5)
+    _, axis = mpplt.subplots()
+    axis.set_xlabel('Rank of Correct Location (%)')
+    axis.set_ylabel('% of Queries')
+    axis.set_title('Localization Results')
+    axis.grid(True, linestyle='--', linewidth=2)
+    axis.spines['top'].set_visible(False)
+    axis.spines['right'].set_visible(False)
+    axis.set_ylim(0, 101)
+    axis.set_xlim(1, 101)
+    axis.plot(location_rank, linewidth=2.5)
     return location_rank
 
--- tests/deit_vis_loc/test_plot.py
+++ tests/deit_vis_loc/test_plot.py
@@ -1,60 +1,66 @@
 #!/usr/bin/env python3
 
-import src.deit_vis_loc.plot as plot
+import collections as cl
 import pytest
 
+import src.deit_vis_loc.plot as plot
 
-def test_ims_of_query_with_n_segments():
+
+def test_im_and_n_closest_segments():
     segments = [
-        {'name': 's3', 'distance': 3, 'is_positive': False},
-        {'name': 's1', 'distance': 1, 'is_positive': True},
-        {'name': 's2', 'distance': 2, 'is_positive': False},
+        {'segment': 's1', 'dist': 1, 'is_pos': True },
+        {'segment': 's2', 'dist': 2, 'is_pos': False},
+        {'segment': 's3', 'dist': 3, 'is_pos': False},
     ]
-    assert list(plot.ims_of_query_with_n_segments(42, 'foo', [])) == [
+    assert list(plot.im_and_n_closest_segments(1, 'foo', []))       == [
             ('foo', 'black', 0)]
-    assert list(plot.ims_of_query_with_n_segments(0,  'foo', segments)) == [
+    assert list(plot.im_and_n_closest_segments(0, 'foo', segments)) == [
             ('foo', 'black', 0)]
-    assert list(plot.ims_of_query_with_n_segments(1,  'foo', segments)) == [
+    assert list(plot.im_and_n_closest_segments(1, 'foo', segments)) == [
             ('foo', 'black', 0), ('s1', 'green', 1)]
-    assert list(plot.ims_of_query_with_n_segments(5,  'foo', segments)) == [
+    assert list(plot.im_and_n_closest_segments(5, 'foo', segments)) == [
             ('foo', 'black', 0), ('s1', 'green', 1), ('s2', 'red', 2), ('s3', 'red', 3)]
 
 
 def test_localization_percentile():
-    with pytest.raises(Exception):
-        plot.localization_percentile([])
-    #^ Query has to always be associated with a positive segment
-
+    with pytest.raises(Exception): plot.localization_percentile([])
+    #^ Query image has to have positive segment
     assert plot.localization_percentile([
-            {'distance': 1, 'is_positive': True},
+            {'dist': 1, 'is_pos': True},
         ]) == 100
     assert plot.localization_percentile([
-            {'distance': 2, 'is_positive': True},
-            {'distance': 1, 'is_positive': False},
-        ]) == 100
+            {'dist': 1, 'is_pos': False},
+            {'dist': 2, 'is_pos': True },
+            {'dist': 3, 'is_pos': False},
+        ]) == 67
     assert plot.localization_percentile([
-            {'distance': 1, 'is_positive': True},
-            {'distance': 2, 'is_positive': False},
-            {'distance': 3, 'is_positive': False},
+            {'dist': 1, 'is_pos': True },
+            {'dist': 2, 'is_pos': False},
+            {'dist': 3, 'is_pos': False},
         ]) == 34
 
 
-def test_percentiles_histogram():
-    with pytest.raises(Exception):
-        plot.running_localization_percentage([])
+def test_histogram():
+    hist = plot.histogram([])
+    assert hist[0] == 0
+    assert hist[1] == 0
 
-    result = plot.running_localization_percentage([1])
-    assert 100 == len(result)
-    assert 100 == result[0]
-    assert all(r == 0 for r in result[1:])
+    hist = plot.histogram([1])
+    assert hist[0] == 0
+    assert hist[1] == 1
 
-    result = plot.running_localization_percentage([2, 1, 1])
-    assert 100 == len(result)
-    assert [66.67, 33.33] == result[0:2]
-    assert all(r == 0 for r in result[2:])
+    hist = plot.histogram([1, 0, 1])
+    assert hist[0] == 1
+    assert hist[1] == 2
 
-    result = plot.running_localization_percentage([2, 1, 1, 3])
-    assert 100 == len(result)
-    assert [50, 25, 25] == result[0:3]
-    assert all(r == 0 for r in result[3:])
+
+def test_iter_histogram_perc():
+    hist = cl.defaultdict(lambda: 0, {})
+    assert list(plot.iter_histogram_perc(hist, range(0))) == []
+    assert list(plot.iter_histogram_perc(hist, range(1))) == []
+
+    hist = cl.defaultdict(lambda: 0, {0: 1, 1: 2})
+    assert list(plot.iter_histogram_perc(hist, range(0))) == []
+    assert list(plot.iter_histogram_perc(hist, range(1))) == [33.33]
+    assert list(plot.iter_histogram_perc(hist, range(2))) == [33.33, 66.67]
 
